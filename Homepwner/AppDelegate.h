c764abec03a43c6c70c8ee80a7b2c18f50bbf2bc
谢海邦 <X@xiehaibangdemacbook-pro.local>
2016-08-23 18:35:16 +0800

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by 谢海邦 on 16/8/23.
+//  Copyright © 2016年 谢海邦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
